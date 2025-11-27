0bcbdc9b3461b856d506a84a8f38a2d4a546a2d4
dewantara <dewantara.tirta@gmail.com>
2025-11-27 13:19:19 +0700

    feat: static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// Using adapter-vercel for Vercel deployment
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: false
+		})
 	}
 };
 
